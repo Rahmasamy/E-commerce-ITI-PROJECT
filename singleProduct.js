dabc019117a2136279ed3197b183ea0c11987d37
mariam <mariemsafwat20@gmail.com>
2024-07-14 00:02:00 +0300

    Make a review of the product and format the date and rating.

--- singleProduct.js
+++ singleProduct.js
@@ -3,10 +3,12 @@ const title = document.getElementById('title');
 const descrip = document.getElementById('description');
 const rate = document.getElementById('rating');
 const price = document.getElementById('price');
+
 const plusBtn = document.getElementById('plus');
 const minusBtn = document.getElementById('minus');
 const countNum = document.getElementById('count');
 
+const reviewboxes = document.getElementById('boxes');
 
 document.addEventListener('DOMContentLoaded', (event) => {
      // Check if we are on the singleProduct.html page
@@ -32,15 +34,69 @@ document.addEventListener('DOMContentLoaded', (event) => {
                     price.innerHTML = `$${product.price}`
 
                     // Fetch rating and convert to stars
-                    fetch(`https://dummyjson.com/products/${productId}`)
-                    .then(res => res.json())
-                    .then(data => {
-                        const starRating = convertRatingToStars(data.rating);
-                        rate.innerHTML = starRating;
+                    const starRating = convertRatingToStars(product.rating);
+                    rate.innerHTML = starRating;
 
-                        console.log('Rating:', starRating);
-                    });
-                                    
+                    console.log('Rating:', starRating); 
+                    for(var i = 0; i < 2; i++){
+                        // reviewBox
+                        const reviewDiv = document.createElement('div');
+                        reviewDiv.id = 'box';
+                        reviewboxes.appendChild(reviewDiv);
+                        
+                        // rating
+                        const rating = document.createElement('p');
+                        rating.id = 'ratingReview';
+                        const starRatingReview = convertRatingToStars(product.reviews[i].rating);
+                        rating.textContent = `${starRatingReview}`;
+                        reviewDiv.appendChild(rating);
+
+                        // comment
+                        const comment = document.createElement('p');
+                        comment.id = 'comment';
+                        comment.textContent = `${product.reviews[i].comment}`;
+                        reviewDiv.appendChild(comment);
+
+                        // create date
+                        const date = document.createElement('h5');
+                        date.id = 'date';
+                        // [0]: return first element of the array after splitted
+                        const splittedDate = product.reviews[i].date.split("T")[0];
+                        // Split the date into components
+                        const [year, month, day] = splittedDate.split("-");
+                        // Format as DD-MM-YYYY
+                        const formattedDate = `${day}-${month}-${year}`;
+                        date.textContent = `${formattedDate}`;
+                        reviewDiv.appendChild(date);
+    
+                        // reviewBox
+                        const info = document.createElement('div');
+                        info.id = 'info';
+                        reviewDiv.appendChild(info);
+                        
+                        // img
+                        const img = document.createElement('img');
+                        img.src = `imgs/avatar1.png`;
+                        img.alt = `profile`;
+                        info.appendChild(img);
+    
+                        // reviewBox
+                        const reviewerInfo = document.createElement('div');
+                        reviewerInfo.id = 'reviewerInfo';
+                        info.appendChild(reviewerInfo);
+
+                        // create date
+                        const name = document.createElement('h');
+                        name.id = 'name';
+                        name.textContent = `${product.reviews[i].reviewerName}`;
+                        reviewerInfo.appendChild(name);
+
+                        // create date
+                        const email = document.createElement('h4');
+                        email.id = 'email';
+                        email.textContent = `${product.reviews[i].reviewerEmail}`;
+                        reviewerInfo.appendChild(email);
+                    }
                 })
                 .catch(error => console.error('Error fetching data:', error));
         }else{
@@ -48,6 +104,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     }
 })
+
 
 // Function to convert rating to stars
 function convertRatingToStars(rating) {
